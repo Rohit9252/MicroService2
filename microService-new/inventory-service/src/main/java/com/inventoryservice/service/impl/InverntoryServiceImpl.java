070dbd6a6e0961c7525ddc982d229f4759dc3f75
Rohit9252 <rohitbatra0165@gmail.com>
2023-04-13 22:19:43 +0530

    notification service created

--- microService-new/inventory-service/src/main/java/com/inventoryservice/service/impl/InverntoryServiceImpl.java
+++ microService-new/inventory-service/src/main/java/com/inventoryservice/service/impl/InverntoryServiceImpl.java
@@ -5,6 +5,8 @@ import com.inventoryservice.model.Inventory;
 import com.inventoryservice.repository.InventoryRepository;
 import com.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +15,16 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InverntoryServiceImpl implements InventoryService {
 
     private  final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
     @Override
     public List<InventoryResponse> isInStock(List<String> skuCode) {
-
+//        log.info("wait Started");
+//        Thread.sleep(10000);
+//        log.info("wait Ended" );
          return inventoryRepository.findBySkuCodeIn(skuCode)
                  .stream().map(this::mapToObj)
                  .collect(Collectors.toList());
--- microService-new/order-service/src/main/java/com/orderservice/controller/OrderConrtoller.java
+++ microService-new/order-service/src/main/java/com/orderservice/controller/OrderConrtoller.java
@@ -2,6 +2,9 @@ package com.orderservice.controller;
 
 import com.orderservice.dtos.OrderRequest;
 import com.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -18,8 +23,16 @@ public class OrderConrtoller {
     private  final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest){
-        return new ResponseEntity<>(orderService.placeOrder(orderRequest), HttpStatus.CREATED);
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallBackMethod")
+//    @TimeLimiter(name="inventory")
+//    @Retry(name = "inventory")
+    public ResponseEntity<CompletableFuture<String>> placeOrder(@RequestBody OrderRequest orderRequest){
+        return new ResponseEntity<>(CompletableFuture.supplyAsync(()-> orderService.placeOrder(orderRequest)), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<CompletableFuture<String>> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+
+        return new ResponseEntity<>(CompletableFuture.supplyAsync(()-> "Oops ! Something went wrong, Please Order after Some Time"), HttpStatus.BAD_GATEWAY);
     }
 
 
